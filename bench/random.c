2fdf271b68a5e592b7044b1d2baf3a34e1d853be
Martin Pépin <kerl@wkerl.me>
2021-07-13 15:22:15 +0200

    Bench: more points

--- bench/random.c
+++ bench/random.c
@@ -21,7 +21,7 @@ static double bench_n(int n, int repeat) {
 }
 
 int main() {
-  const int n = 1e7, nbpts = 50, step = n / nbpts, repeat = 100;
+  const int n = 1e7, nbpts = 100, step = n / nbpts, repeat = 100;
 
   for (int k = step; k <= n; k += step) {
     double res = bench_n(k, repeat);
